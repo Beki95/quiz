23ca9018a369dc99f26e45561deabc4b254bd471
Beki95 <imanjibeki@gmail.com>
2022-05-05 16:23:48 +0600

    added schematics

---
+++ schemas/questions_scheme.py
@@ -0,0 +1,21 @@
+import datetime
+from datetime import datetime as dt
+
+from pydantic import BaseModel
+
+
+class QuestionCount(BaseModel):
+    questions_num: int
+
+
+class QuestionScheme(BaseModel):
+    id: int
+    question: str
+    answer: str
+    created_at: datetime.datetime = dt.utcnow()
+    question_created_at: datetime.datetime = dt.utcnow()
+
+
+class QuizScheme(BaseModel):
+    id: int
+    created_at: datetime.datetime
