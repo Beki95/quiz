a53abf1899ba54dc5a762a3654f382568d6023ba
Beki95 <imanjibeki@gmail.com>
2022-05-05 16:15:27 +0600

    change endpoints.py

--- apps/questions/endpoints.py
+++ apps/questions/endpoints.py
@@ -1,8 +1,17 @@
 from fastapi import APIRouter
+from fastapi import Depends
+
+from core.base_db import get_db
+from core.tasks import task_get_questions
+from repositories.quiz import QuizRepository
+from schemas.questions_scheme import QuestionCount
 
 router = APIRouter()
 
 
 @router.post("/")
-async def create_questions():
-    return {"questions_num": "Hello World"}
+async def create_questions(count: QuestionCount, db=Depends(get_db)):
+    count = count.dict().get('questions_num')
+    data = await QuizRepository(db=db).get()
+    task_get_questions.delay(count)
+    return data
